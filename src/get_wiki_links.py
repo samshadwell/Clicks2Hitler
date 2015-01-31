6c4a36de0504949f5571dc9fcbaba722e170de6f
Samuel Shadwell <smshdwll@gmail.com>
2015-01-31 17:30:22 -0600

    Changed filenames to fit python naming conventions

--- src/get_wiki_links.py
+++ src/get_wiki_links.py
@@ -1,6 +1,6 @@
 __author__ = 'github.com/samshadwell'
 
-def get_wiki_links(url_in):
+def get_links(url_in):
     """
     Get all the links to wikipedia pages at the given url
     :param url_in: url to get links from
