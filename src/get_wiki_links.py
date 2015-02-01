91f517374cc655a109bbf4eea14c7b080f87028c
jtschneck <jts7@rice.edu>
2015-01-31 20:52:05 -0600

    Completed get_links

--- src/get_wiki_links.py
+++ src/get_wiki_links.py
@@ -1,8 +1,27 @@
 __author__ = 'github.com/samshadwell'
 
-def get_links(url_in):
+import urllib.request
+import re
+
+def get_links(url_in, regex):
     """
     Get all the links to wikipedia pages at the given url
     :param url_in: url to get links from
-    :return: list of links in arbitrary order
+    :return: set of links in arbitrary order
     """
+
+    resp = urllib.request.urlopen(url_in)
+    html = str(resp.read())
+
+    links_set = set([])
+
+    for link in regex.findall(html):
+        links_set.add("http://en.wikipedia.org" + link.split("\"")[1])
+
+    return links_set
+
+
+regex = re.compile('(?:a href=("\/wiki\/[^:]*?"))')
+
+
+
